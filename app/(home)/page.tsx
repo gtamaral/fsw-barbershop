ad6644ea0b9157c5add304fb99caab33e6796032
gtamaral <rodrigoamaral003@gmail.com>
2024-01-31 20:34:50 -0300

    feat: add footer component

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -47,6 +47,16 @@ export default async function Home() {
           ))}
         </div>
       </div>
+
+      <div className="mt-6 mb-[4.5rem]">
+        <h2 className="text-xs px-5 mb-3 uppercase text-gray-400 font-bold">Populares</h2>
+
+        <div className="flex gap-2 px-5 overflow-x-auto [&::-webkit-scrollbar]:hidden">
+          {barbershops.map((barbershop) => (            
+              <BarbershopItem key={barbershop.id} barbershop={barbershop}/>
+          ))}
+        </div>
+      </div>
      </div>
   );
 }
