9c2a348feba406e40de666a57aa78cd728de48c4
gtamaral <rodrigoamaral003@gmail.com>
2024-01-31 01:14:36 -0300

    feat: add welcome message

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -1,10 +1,22 @@
 import Image from "next/image";
 import Header from "../_components/header";
 
+// datefns
+import {format} from "date-fns"
+import {ptBR} from "date-fns/locale"
+
 export default function Home() {
   return (
      <div>
       <Header />
+
+      <div className="px-5 pt-5">
+        <h2 className="text-xl font-bold">Olá, Rodrigo!</h2>
+      <p className="capitalize text-sm">{format(new Date(), "EEEE','d 'de' MMMM",{
+        locale: ptBR,
+      }
+      )}</p>
+      </div>
      </div>
   );
 }
