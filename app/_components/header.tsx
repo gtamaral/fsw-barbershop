747362e3d377d8d9cb7aa8510bef52455e259f55
gtamaral <rodrigoamaral003@gmail.com>
2024-02-02 12:49:42 -0300

    feat: add sidemenu

--- app/_components/header.tsx
+++ app/_components/header.tsx
@@ -3,12 +3,10 @@
 import Image from "next/image"
 import {Card, CardContent} from "./ui/card"
 import {Button} from "./ui/button"
-import { CalendarIcon, HomeIcon, LogInIcon, LogOutIcon, MenuIcon, UserIcon } from "lucide-react";
+import { MenuIcon } from "lucide-react";
 import { signIn, signOut, useSession } from "next-auth/react";
-import { Sheet, SheetContent, SheetHeader, SheetTitle, SheetTrigger } from "./ui/sheet";
-import { Avatar } from "@radix-ui/react-avatar";
-import { AvatarImage } from "./ui/avatar";
-import Link from "next/link";
+import { Sheet, SheetContent, SheetTrigger } from "./ui/sheet";
+import SideMenu from "./side-menu";
 
 const Header = () => {
 
@@ -34,62 +32,11 @@ const Header = () => {
                     </SheetTrigger>
 
                     <SheetContent className="p-0">
-                        <SheetHeader className="text-left border-b border-solid border-secondary p-5">
-                            <SheetTitle>
-                                <h1 className="text-2xl font-bold">Menu</h1>
-                            </SheetTitle>
-                        </SheetHeader>
-
-                        {data?.user ? (
-                            <div className="flex justify-between px-5 py-6 items-center">
-                                <div className="flex items-center gap-3">
-                                <Avatar>
-                                    <AvatarImage src={data.user?.image ?? ''} />
-                                </Avatar>
-
-                                <h2 className="font-bold">{data.user.name}</h2>
-                                </div>
-
-                                <Button variant="secondary" size="icon">
-                                    <LogOutIcon onClick={handleLogoutClick}/>
-                                </Button>
-
-                            </div>
-                            
-                        ) : (
-                            <div className="flex flex-col px-5 py-6 gap-3">
-                                <div className="flex items-center gap-2">
-                                    <UserIcon size={32}/>
-                                    <h2 className="font-bold">Olá, faça seu login!</h2>
-                                </div>
-
-                                <Button variant="secondary" className="w-full justify-start" onClick={handleLoginClik}>
-                                    <LogInIcon className="mr-2" size={18} />
-                                    Fazer Login
-                                </Button>
-                            </div>
-                        )}
-
-                        <div className="flex flex-col gap-3 px-5">
-                            <Button variant="outline" className="justify-start" asChild>
-                                <Link href="/">
-                                <HomeIcon size={18} className="mr-2"/>
-                                Inicio
-                                </Link>
-                            </Button>
-
-                            {data?.user && (
-                                <Button variant="outline" className="justify-start">
-                                <CalendarIcon size={18} className="mr-2"/>
-                                Agendamentos
-                                </Button>
-                            )}
-                        </div>
+                        <SideMenu />
                     </SheetContent>
-                </Sheet>
 
-                {/* <Button onClick={handleLoginClik}>login</Button> */}
-                
+                    
+                </Sheet>                
             </CardContent>
         </Card>
      );
--- app/barbershop/[id]/components/barbershop-info.tsx
+++ app/barbershop/[id]/components/barbershop-info.tsx
@@ -6,6 +6,8 @@ import Image from "next/image";
 import { db } from "@/app/_lib/prisma"
 import { Barbershop } from "@prisma/client";
 import { useRouter } from "next/navigation";
+import { SheetContent, SheetTrigger, Sheet } from "@/app/_components/ui/sheet";
+import SideMenu from "@/app/_components/side-menu";
 
 interface BarbershopInfoProps {
     barbershop: Barbershop
@@ -31,6 +33,20 @@ const BarbershopInfo = ({barbershop}: BarbershopInfoProps) => {
             <Button size="icon" variant="outline" className="z-50 top-4 right-4 absolute" >
                 <MenuIcon  />
             </Button>
+
+            <Sheet>
+                    <SheetTrigger asChild>
+                        <Button size="icon" variant="outline" className="z-50 top-4 right-4 absolute" >
+                            <MenuIcon  />
+                        </Button>
+                    </SheetTrigger>
+
+                    <SheetContent className="p-0">
+                        <SideMenu />
+                    </SheetContent>
+
+                    
+            </Sheet> 
 
             <Image
              src={barbershop.imageUrl} 
